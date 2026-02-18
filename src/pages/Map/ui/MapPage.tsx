99d9d2ad58775a8212f73c2de28f7bc2e93080aa
PlexMAD <113678445+PlexMAD@users.noreply.github.com>
2026-02-18 23:41:16 +0300

    init deckgl

--- src/pages/Map/ui/MapPage.tsx
+++ src/pages/Map/ui/MapPage.tsx
@@ -1,10 +1,45 @@
-import { Button } from "@/shared/ui/button";
+import { GeoJsonLayer } from "@deck.gl/layers";
+import { DeckGL } from "@deck.gl/react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import "maplibre-gl/dist/maplibre-gl.css";
+import { Map } from "react-map-gl/maplibre";
+
+const INITIAL_VIEW_STATE = {
+  longitude: 37.6176,
+  latitude: 55.7558,
+  zoom: 10,
+};
 
 export const MapPage = () => {
+  const { data: metros } = useQuery({
+    queryKey: ["metro"],
+    queryFn: async () => {
+      const result = await axios.get("/api/metro.json");
+      return result.data;
+    },
+  });
+
+  console.log(metros);
+
+  const layers = [
+    new GeoJsonLayer({
+      id: "metro-layer",
+      data: metros,
+      pickable: true,
+      pointType: "circle",
+      getPointRadius: 50,
+      getFillColor: [255, 0, 0, 180],
+      getLineColor: [0, 0, 0, 200],
+      lineWidthMinPixels: 1,
+    }),
+  ];
+
   return (
-    <div>
-      <div>Привет</div>
-      <Button onClick={() => console.log("Привет")}>Привет</Button>
+    <div style={{ height: "100vh" }}>
+      <DeckGL initialViewState={INITIAL_VIEW_STATE} controller layers={layers}>
+        <Map mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json" />
+      </DeckGL>
     </div>
   );
 };
